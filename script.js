3a104c20ff41977691aacb54a687b5f4a07167da
oLucasGregory <l.gregory1254@gmail.com>
2023-02-05 13:14:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const miliseg = document.querySelector('.milissegundos')
+const seg = document.querySelector('.segundos')
+const min = document.querySelector('.minutos')
+
+let miliNum = 0
+let segNum = 0
+let minNum = 0
+let INTERVALO
+
+function milissegundos() {
+    miliNum ++
+    if(miliNum < 10){
+        miliseg.innerHTML = '0'+ miliNum
+    }
+    else{
+        miliseg.innerHTML = miliNum
+    }
+    if (miliNum == 99){
+        miliNum = 0
+        segundos()
+    }
+    miliseg.innerHTML = miliNum
+
+    }
+function segundos() {
+    segNum++
+    if(segNum < 10){
+        seg.innerHTML = '0' + segNum
+    }
+    else{
+        seg.innerHTML = segNum
+    }
+    if (segNum == 59) {
+    segNum = 0
+    minutos()        
+    }
+    seg.innerHTML = segNum
+    
+}
+function minutos() {
+    minNum++
+    min.innerHTML = minNum
+    
+}
+function iniciar() {
+    clearInterval(INTERVALO)
+    INTERVALO = setInterval(() => {milissegundos()}, 10)
+    
+}
+function parar() {
+    clearInterval(INTERVALO)
+    
+}
+function resetar() {
+    clearInterval(INTERVALO)
+miliNum = 0
+segNum = 0
+minNum = 0
+miliseg.innerHTML = '00'
+seg.innerHTML = '00'
+min.innerHTML = '00'    
+}
